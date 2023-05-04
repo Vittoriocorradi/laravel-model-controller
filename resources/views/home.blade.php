8ff846e526669ba3001c8c94938f499d114ca1e7
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-05-04 15:20:56 +0200

    Stilizzazione

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,15 +6,15 @@
 
 @section('page-main')
     <section class="movie-list">
-        <div class="row big-container">
+        <div class="row big-container g-4">
             @foreach ($movies as $movie)
-                <div class="col-6 col-sm-4 col-lg-3">
+                <div class="col-6 col-md-4 col-xl-3">
                     <div class="card ms-card">
                         <h2 class="card-header">{{ $movie->title }}</h2>
                         <div class="card-body">
                         <h3 class="card-title">Titolo originale: {{ $movie->original_title }}</h3>
                         <div>Nazionalità: {{ $movie->nationality }}</div>
-                        <div>Data di rilascio: {{ $movie->date }}</div>
+                        <div>Data di rilascio: {{ date('d-M-y', strtotime($movie->date)) }}</div>
                         <div>Punteggio: {{ $movie->vote }}</div>
                         </div>
                     </div>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,5 @@
+<header>
+    <div class="big-container">
+        <h1 class="pb-5 text-uppercase">my movies</h1>
+    </div>
+</header>
