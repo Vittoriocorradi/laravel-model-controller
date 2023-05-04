5149732049d36177c9b04adc75906888ac877010
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-05-04 14:45:48 +0200

    Layout cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,25 @@
 @extends('layouts.app')
 
 @section('page-title')
-    Template
+    Laravel Model Controller
 @endsection
 
 @section('page-main')
-    <h1>Template</h1>
+    <section class="movie-list">
+        <div class="row big-container">
+            @foreach ($movies as $movie)
+                <div class="col-6 col-sm-4 col-lg-3">
+                    <div class="card ms-card">
+                        <h2 class="card-header">{{ $movie->title }}</h2>
+                        <div class="card-body">
+                        <h3 class="card-title">Titolo originale: {{ $movie->original_title }}</h3>
+                        <div>Nazionalità: {{ $movie->nationality }}</div>
+                        <div>Data di rilascio: {{ $movie->date }}</div>
+                        <div>Punteggio: {{ $movie->vote }}</div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </section>
 @endsection
