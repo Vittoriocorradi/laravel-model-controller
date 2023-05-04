493ad84b6176460cc99c359207e41e427d77936f
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-05-04 13:58:30 +0200

    Dati passati alla view

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,8 +4,21 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
-{
-    //
+{    
+    /**
+     * index
+     * 
+     * Controller per la view 'home' (Pagina principale)
+     *
+     * @return void
+     */
+    public function index() {
+
+        $movies = Movie::all();     //Recupero tutti i dati dalla tabella movies
+
+        return view('home', compact('movies'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Guest\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+
+// Route per la pagina principale passando dal controller
+
+Route::get('/', [PageController::class, 'index'])->name('home');
